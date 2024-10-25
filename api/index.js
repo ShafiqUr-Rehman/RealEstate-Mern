d09802200782ba756227f3f25759016bba186d6a
Shafiq Ur Rehman <shafiqurrehmanbscs2022@gmail.com>
2024-10-25 07:23:14 +0500

    done

--- api/index.js
+++ api/index.js
@@ -13,7 +13,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://real-estate-mern-frontend-one.vercel.app/','http://localhost:3000'], 
+  credentials: true,
+  optionsSuccessStatus: 200, 
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
