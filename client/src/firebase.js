4a24f99a02d69efad3d268c70f62cde1141c9df6
Shafiq Ur Rehman <shafiqurrehmanbscs2022@gmail.com>
2025-01-11 10:09:36 +0500

    dockerization of lms project

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   projectId: "mern-estate-ccf43",
   storageBucket: "mern-estate-ccf43.appspot.com",
   messagingSenderId: "765451884672",
-  appId: "1:765451884672:web:1c72f94ba28ba84b09ae65"
+  appId: "1:765451884672:web:1c72f94ba28ba84b09ae65",
 };
 
 // Initialize Firebase
